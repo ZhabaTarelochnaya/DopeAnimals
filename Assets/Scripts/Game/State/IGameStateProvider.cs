94679a658133dde53190835a729e5dd18486dd48
ZhabaTarelochnaya <140906788+ZhabaTarelochnaya@users.noreply.github.com>
2025-03-13 21:25:31 +0300

    Saving and loading

--- Assets/Scripts/Game/State/IGameStateProvider.cs
+++ Assets/Scripts/Game/State/IGameStateProvider.cs
@@ -3,4 +3,8 @@
 public interface IGameStateProvider
 {
     public GameStateProxy GameState { get; }
+
+    public Observable<GameStateProxy> LoadGameState();
+    public Observable<bool> SaveGameState();
+    public Observable<bool> ResetGameState();
 }
--- Assets/Scripts/Game/State/PlayerPrefsGameStateProvider.cs
+++ Assets/Scripts/Game/State/PlayerPrefsGameStateProvider.cs
@@ -1,10 +1,56 @@
-﻿using System;
+﻿using R3;
 using System.Collections.Generic;
-using System.Net.NetworkInformation;
 using UnityEngine;
 
 public class PlayerPrefsGameStateProvider : IGameStateProvider
 {
+    const string GAME_STATE_KEY = nameof(GAME_STATE_KEY);
+    const string GAME_SETTINGS_STATE_KEY = nameof(GAME_SETTINGS_STATE_KEY);
 
+    GameState _gameStateOrigin;
     public GameStateProxy GameState { get; private set; }
+
+    public Observable<GameStateProxy> LoadGameState()
+    {
+        if (!PlayerPrefs.HasKey(GAME_STATE_KEY))
+        {
+            GameState = CreateGameStateFromSettings();
+            Debug.Log("Game State created from settings: " + JsonUtility.ToJson(_gameStateOrigin, true));
+
+            SaveGameState();    // Сохраним дефолтное состояние
+        }
+        else
+        {
+            // Загружаем
+            var json = PlayerPrefs.GetString(GAME_STATE_KEY);
+            _gameStateOrigin = JsonUtility.FromJson<GameState>(json);
+            GameState = new GameStateProxy(_gameStateOrigin);
+
+            Debug.Log("Game State loaded: " + json);
+        }
+
+        return Observable.Return(GameState);
+    }
+    public Observable<bool> SaveGameState()
+    {
+        var json = JsonUtility.ToJson(_gameStateOrigin, true);
+        PlayerPrefs.SetString(GAME_STATE_KEY, json);
+
+        return Observable.Return(true);
+    }
+
+    public Observable<bool> ResetGameState()
+    {
+        GameState = CreateGameStateFromSettings();
+        SaveGameState();
+
+        return Observable.Return(true);
+    }
+
+    private GameStateProxy CreateGameStateFromSettings()
+    {
+        // Settings needed
+        _gameStateOrigin = new GameState();
+        return new GameStateProxy(_gameStateOrigin);
+    }
 }
